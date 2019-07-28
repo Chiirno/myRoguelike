9f0f79e7494d789cb142238f45a2fcedf609c425
Cirno <GeniusChiruno@gmail.com>
2019-07-28 01:52:53 -0300

    added a buffer and made the screen black

--- driver/Game.java
+++ driver/Game.java
@@ -5,6 +5,9 @@
  */
 package myRoguelike.driver;
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import mobs.*;
 
 
@@ -68,7 +71,19 @@ public class Game extends Canvas implements Runnable {
     }
     
     private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
         
+        Graphics g = bs.getDrawGraphics();
+        
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        g.dispose();
+        bs.show();
     }
     
     public static void main(String[] args){
