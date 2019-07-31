646eeeab8011a4a67461d22079c671806b684a12
Cirno <GeniusChiruno@gmail.com>
2019-07-31 04:32:48 -0300

    turned the 1d arraylist into an easier to handle 2d arraylist for map tiles

--- driver/Grid.java
+++ driver/Grid.java
@@ -13,37 +13,42 @@ import javax.swing.*;
 public class Grid 
 { 
     JFrame frame; 
-  
-    static int indexGetter(int x, int y) {
-        return 15*x + y;
-    }
-    
 
     
     Grid() 
     { 
         ArrayList<JButton> tileArr = new ArrayList<JButton>();
         
+        int gridSize = 75;
+        int tileSize = (750/gridSize);
+        JButton[][] gridTiles = new JButton[gridSize][gridSize];
+        
         // creating instance of JFrame with name "first way" 
         frame = new JFrame("first way");
           
         // creates instance of JButton  
         
-        
-        int n = 0;
-        for (int x = 0; x < 30; x++) {
-            for (int y = 0; y < 30; y++) {
-                tileArr.add(new JButton());
-                tileArr.get(n).setBounds(25*x+1, 25*y+1, 25, 25);
-                frame.add(tileArr.get(n));
-                System.out.println("(" + x + "," + y + ")");
-                n++;
+        for (int x = 0; x < gridSize; x++) {
+            for (int y = 0; y < gridSize; y++) {
+                JButton newTile = new JButton();
+                newTile.setBounds(tileSize*x+1, tileSize*y+1, tileSize, tileSize);
+                gridTiles[x][y] = new JButton();
+                frame.add(newTile);
             }
         }
         
-        
-        
-        tileArr.get(indexGetter(0,0)).setBackground(Color.RED);
+//        
+//        int n = 0;
+//        for (int x = 0; x < 30; x++) {
+//            for (int y = 0; y < 30; y++) {
+//                tileArr.add(new JButton());
+//                tileArr.get(n).setBounds(25*x+1, 25*y+1, 25, 25);
+//                frame.add(tileArr.get(n));
+//                //System.out.println("(" + x + "," + y + ")");
+//                n++;
+//            }
+//        }
+
         
         
 //        buttonx.setBounds(100, 100, 50, 50);
